0b758dbd251fecffc731b8ed50be4eb5992264d8
Jonny007-MKD <nospam@jonny007-mkd.de>
2020-02-16 18:24:25 +0100

    Definition format is now JSON

--- Settings.py
+++ Settings.py
@@ -1,17 +1,15 @@
-import csv
+import json
 
 class Settings:
     def __init__(self, defpath):
         with open(defpath,'r') as deffile:
-            csvdeff = csv.reader(deffile, delimiter=',')
-            next(csvdeff, None) # ignore first line
-            for settings in csvdeff:
-                self.date       = int(settings[0])  # convert to int
-                self.dateformat = settings[1]
-                self.amount     = int(settings[2])  # How much was the transaction
-                self.memo       = int(settings[3])  # discription of the transaction
-                self.payee      = int(settings[4])  # Where the money is going
-                self.delimiter  = settings[5]       # How the csv is separated
-                self.header     = int(settings[6])  # Set if there is a header to skip
-                self.encoding   = settings[7]
-                self.locale     = settings[8]
+            deff = json.load(deffile)
+            self.dateformat  = deff["meta"]["dateformat"]
+            self.delimiter   = deff["meta"]["delimiter"]
+            self.encoding    = deff["meta"]["encoding"]
+            self.locale      = deff["meta"]["locale"]
+            self.header      = int(deff["meta"]["header"])
+            self.date        = int(deff["columns"]["date"])
+            self.amount      = int(deff["columns"]["amount"])
+            self.memo        = int(deff["columns"]["memo"])
+            self.payee       = int(deff["columns"]["payee"])
