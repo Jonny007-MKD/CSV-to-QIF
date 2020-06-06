8a85f924098bb904478977dba581825dfae98334
Jonny007-MKD <nospam@jonny007-mkd.de>
2020-06-06 17:08:19 +0200

    Improved example convert.py

--- convert.py
+++ convert.py
@@ -6,11 +6,32 @@ def convertToTransaction(date, amount, memo, payee):
     t.amount = amount
     t.memo = memo
     t.payee = getPayee(date, amount, memo, payee)
-    t.category = getCategory(date, amount, memo, payee)
+    if t.payee == None: return None
+
+    t.category = getCategory(date, amount, memo, payee, t.payee)
+
     return t
 
 def getPayee(date, amount, memo, payee):
+    if memo == "Tagessaldo": return None
+
+    for a, b in payeeToPayee.items():
+        if a in payee.lower(): return b
+
     return payee
 
-def getCategory(date, amount, memo, payee):
+def getCategory(date, amount, memo, payeeOld, payeeNew):
+    if payeeNew in payeeNewToCategory: return payeeNewToCategory[payeeNew]
+
+    if "amazon web" in payeeOld.lower(): return "Sonstige Ausgaben:Backup";
     return None
+
+payeeNewToCategory = {
+    "McDonalds": "Lebensmittel:Imbiss",
+}
+
+payeeToPayee = {
+    "mcdonald": "McDonalds",
+    "mc donald": "McDonalds",
+}
+
